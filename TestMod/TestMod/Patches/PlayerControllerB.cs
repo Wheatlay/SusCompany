9e8e3c868ffadf522056b607d3292c8d0eca4682
Krzysztof Wołkowycki <krzyswol2000@gmail.com>
2023-12-24 15:02:41 +0100

    Dodano funkcje testowania statusu impostora (kucanie - skok) Dodano czerwone nicki widoczne pomiędzy impostorami

--- TestMod/TestMod/Patches/PlayerControllerB.cs
+++ TestMod/TestMod/Patches/PlayerControllerB.cs
@@ -14,28 +14,57 @@ namespace TestMod.Patches
     [HarmonyPatch(typeof(PlayerControllerB))]
     class PlayerControllerBPatch
     {
+
+        [HarmonyPatch("Crouch")]
+        [HarmonyPostfix]
+        static public void GiveImpostorTest()
+        {
+            int a = 111111111;
+            int b = 4;
+            StartOfRoundPatch.ImpostorStartGame(ref a,ref b);
+        }
+        [HarmonyPatch("LandFromJumpClientRpc")]
+        [HarmonyPostfix]
+        static public void RemoveImposterTest()
+        {
+            OtherFunctions.RemoveImposter();
+        }
+
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
         static public void PlayerControllerUpdate()
         {
-            /*  TestModBase.mls.LogInfo("Player.ActiveList.Count is : " + Player.ActiveList.Count);
-              IEnumerator<Player> activePlayers = Player.ActiveList.GetEnumerator();
-              while (activePlayers.MoveNext())
-              {
-                  TestModBase.mls.LogInfo("Username is :" + activePlayers.Current.Username + " ClientId is : " + activePlayers.Current.ClientId);
-              }
-              TestModBase.mls.LogInfo("StartOfRound.Instance.ClientPlayerList.Count is: "+StartOfRound.Instance.ClientPlayerList.Count);*/
+            
+
+            IEnumerator<Player> activePlayers = Player.ActiveList.GetEnumerator();
+            while (activePlayers.MoveNext())
+            {
+                if (TestModBase.impostorsIDs.Contains((int)activePlayers.Current.ClientId) && activePlayers.Current.IsControlled && 
+                    (TestModBase.impostorsIDs.Contains((int)Player.LocalPlayer.ClientId)))
+                {
+                    activePlayers.Current.PlayerController.usernameBillboardText.canvasRenderer.SetColor(UnityEngine.Color.red);
+                    activePlayers.Current.PlayerController.usernameAlpha.alpha = 1f;
+                }
+                else
+                {
+                    activePlayers.Current.PlayerController.usernameBillboardText.canvasRenderer.SetColor(UnityEngine.Color.white);
+                }
+            }
+
             try
             {
                 if (TestModBase.impostorsIDs.Contains((int)Player.LocalPlayer.ClientId))
                 {
                     Player.LocalPlayer.PlayerController.sprintMeter = 1f;
                 }
+
             }
             catch
             {
                 TestModBase.mls.LogInfo("Failed to get Player.LocalPlayer.ClientId");
             }
+
+
         }
 
         [HarmonyPatch("KillPlayerClientRpc")]
